b0756609b4d30b39b8c1e07f4f227d58683dacaa
myahartman <hartmanmya@gmail.com>
2020-11-08 16:34:01 -0600

    Mya has joined the chat

---
+++ tests/java/com/datastructures/LoginTest.java
@@ -0,0 +1,20 @@
+package com.datastructures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginTest {
+
+    @Test
+    void signUp() {
+    }
+
+    @Test
+    void userExists() {
+    }
+
+    @Test
+    void login() {
+    }
+}
