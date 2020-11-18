ad62632fe45befab2bff381762c59a11343d3c13
myahartman <hartmanmya@gmail.com>
2020-11-18 12:53:34 -0600

    added myas grades

--- src/com/datastructures/screens/StudentScreen.java
+++ src/com/datastructures/screens/StudentScreen.java
@@ -3,6 +3,7 @@ package com.datastructures.screens;
 import com.datastructures.GradesManager;
 import com.datastructures.LoginManager;
 import com.datastructures.MainPortal;
+import com.datastructures.Objects.Grade;
 import com.datastructures.interfaces.Screen;
 
 public class StudentScreen extends MainPortal implements Screen {
@@ -16,14 +17,18 @@ public class StudentScreen extends MainPortal implements Screen {
         print("1. View your grades  2. Allow a parent to view your grades  3. Log out");
         switch (requestData("choice",3)){
             case "1" ->{
-                gradesManager.getGrade()
-
+                int number = 1;
+                print("   Name | Grade   ");
+                for(Grade grade : gradesManager.getAllGrades()){
+                    print("_. _ _",number,grade.getName(),grade.getGrade());
+                    number++;
+                }
             }
             case "2" ->{
 
             }case "3" ->{
 
             }
-        }//add
-    }////add
+        }
+    }
 }
